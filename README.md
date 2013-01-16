4dcc21a21045a4b72aa904d32875f93ef37be88d
mgbaozi <wuhao930301@126.com>
2013-01-16 12:06:37 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 sendemail
 =========
+python sendemail.py -t 收件人 -s 主题 -m 内容 -f 附件
 
+如果有多个收件人请用空格隔开，多个附件同
+
+请事先修改配置文件
 send email with console
---
+++ sendemail.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#coding: utf-8
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+import os
+import sys
+from ConfigParser import ConfigParser
+class SendEmail(object):
+	def __init__(self):
+		config_file = sys.path[0] + os.path.sep +'mail.conf'
+		conf = ConfigParser()
+		conf.read(config_file)
+		self.mail_host = conf.get('server','host')
+		self.mail_user = conf.get('client','account')
+		self.mail_pass = conf.get('client','passwd')
+		name = conf.get('client','name')
+		try:
+			address = conf.get('client','address')
+		except:
+			address = mail_user + '@' + mail_host[5:]
+		self.me = name + '<' + address + '>'
+
+	def _build_msg(self,args):
+		to_list = args['t']
+		msg = MIMEMultipart()
+		msg['From'] = self.me
+		msg['Subject'] = args['s']
+		msg['To'] = ';'.join(to_list)
+		msg.attach(MIMEText(args['m'], 'plain','utf-8'))
+		for file in args['f']:
+			part = MIMEBase('application','octet-stream')
+			part.set_payload(open(file,'rb').read())
+			encoders.encode_base64(part)
+			part.add_header('Content-Disposition', 'attachment; filename="%s"' %os.path.basename(file))
+			msg.attach(part)
+		return msg
+
+	def send(self,args):
+		msg = self._build_msg(args)
+		send_smtp = smtplib.SMTP()
+		send_smtp.connect(self.mail_host)
+		send_smtp.login(self.mail_user,self.mail_pass)
+		send_smtp.sendmail(self.me,args['t'],msg.as_string())
+		send_smtp.close()
+
+if __name__ == '__main__':
+	argv = sys.argv
+	argv.pop(0)
+	args = {'t':[],'m':'','s':'','f':[]}
+	for item in argv:
+		if '-' in item:
+			flag = item[1]
+			continue
+		if isinstance(args[flag],list):
+			args[flag].append(item)
+		else:
+			args[flag] += item
+	SendEmail().send(args)
+	print u'发送成功'
